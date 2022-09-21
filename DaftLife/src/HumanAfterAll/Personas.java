44987d08790f9dcab2eb2d0dd568767b181e2b35
CasperDevs <casperdevs@outlook.es>
2022-09-20 23:50:18 -0500

    2

---
+++ DaftLife/src/HumanAfterAll/Personas.java
@@ -0,0 +1,20 @@
+package HumanAfterAll;
+
+public class Personas {
+    String nombre;
+    byte edad;
+    byte numPiernas;
+    byte numBrazos;
+    String colorOjos;
+
+    //Constructor
+    //inicializar la variable
+
+    public Personas(){
+        this.nombre = "Thomas Bangalter";
+        this.numBrazos = 2;
+        this.numPiernas = 2;
+        this.colorOjos = "Verde";
+
+    }
+}
---
+++ DaftLife/src/HumanAfterAll/Principal.java
@@ -0,0 +1,14 @@
+package HumanAfterAll;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        //Instanciamos un objeto de la clase persona
+        Personas obj1 = new Personas();
+
+        System.out.println("El nombre que contiene objeto 1 es :"+ obj1.nombre+ "La edad es :"+obj1.edad+
+                "El color de ojos es:"+obj1.colorOjos+"Tiene :"+obj1.numPiernas+"Piernas"+"Tiene :"+
+                obj1.numBrazos+"Brazossss");
+
+    }
+}
