649f5e9ae7e66f119adce5f779637bc5ab2ef94a
CasperDevs <casperdevs@outlook.es>
2022-09-26 18:44:20 -0500

    changes at @casperdevs

--- DaftLife/src/Personas/Personas.java
+++ DaftLife/src/Personas/Personas.java
@@ -6,6 +6,7 @@ public class Personas {
     byte NumeroPiernas;
     byte NumeroBrazos;
     String ColorOjos;
+    byte vidaRestante;
 
     //Constructor
     public Personas(){
@@ -14,7 +15,7 @@ public class Personas {
         this.NumeroPiernas = 2;
         this.NumeroBrazos = 2;
         this.ColorOjos = "Bleizer";
-
+        this.vidaRestante = 100;
     }
 
     public  Personas(String nombre ,byte edad, byte NumeroPiernas, byte NumeroBrazos ,String ColorOjos){
@@ -23,7 +24,24 @@ public class Personas {
         this.NumeroPiernas = NumeroPiernas;
         this.NumeroBrazos = NumeroBrazos;
         this.ColorOjos = ColorOjos;
+        this.vidaRestante = 100;
     }
+
+    public String Saludar(){
+        return "Hola mi Nombre es "+this.nombre;
+    }
+
+    public void CambiarNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+     public void Enfermar(int PuntosVida){
+        this.vidaRestante -= PuntosVida;
+     }
+
+     public void Comer(int Comida){
+        this.vidaRestante += Comida;
+     }
 
 
 }
--- DaftLife/src/Personas/Principal.java
+++ DaftLife/src/Personas/Principal.java
@@ -5,16 +5,38 @@ public class Principal {
         //Crear o instanciar objeto de la clase personas
 
         Personas objeto1 = new Personas();
-        System.out.println("El nombre que contiene objeto1 es:" +"   "+objeto1.nombre   + "   " +"La edad es: " + "  " +objeto1.edad + "Años"
-           +"   " + "El color de ojos es: " +objeto1.ColorOjos + "   "+"Tiene :" +
+        System.out.println("El nombre que contiene objeto1 es:" + "   " + objeto1.nombre + "   " + "La edad es: " + "  " + objeto1.edad + "Años"
+                + "   " + "El color de ojos es: " + objeto1.ColorOjos + "   " + "Tiene :" +
                 objeto1.NumeroBrazos + "   " +
-             "Brazos" +"    "  + "Y " +"     " +"Tiene : "+objeto1.NumeroPiernas + "Piernas" );
+                "Brazos" + "    " + "Y " + "     " + "Tiene : " + objeto1.NumeroPiernas + "Piernas");
 
+        Personas objeto2 = new Personas("Chase Coleman", (byte) 26, (byte) 2, (byte) 2, "Concavo");
+        System.out.println("El nombre que tiene objeto 2 es:" + "  " + objeto2 + " " + "El color de Ojos que tiene el objeto 2 es:"
+                + "  " + objeto2.ColorOjos + " " + "Tiene :" + "  " + objeto2.NumeroBrazos + " " + "Brazos" + " " + " y" + " " + "Tiene : " + "  " + objeto2.NumeroPiernas + " "
+                + "Piernas");
 
+        System.out.println("El saludo de objeto 1 es:\n" + objeto1.Saludar());
+        System.out.println("El saludo de objeto 1 es:\n" + objeto2.Saludar());
 
+        objeto2.CambiarNombre("Ydelahina Moritz Crhue");
+        System.out.println("El saludo de objeto 1 Despues de suistituir elemneto es:\n" + objeto2.Saludar());
+        objeto2.CambiarNombre("Thurko Troitz Herrer");
 
+        System.out.println("objecto 2 Says:Hi my name is:"+" "+objeto2.Saludar());
 
+        System.out.println("La vida del objeto 2 antes de enfermar es: \n"+objeto2.vidaRestante);
+        objeto2.Enfermar(20);
+        System.out.println("La vida del objeto 2 despues de enfermar con 20puntos es:\n"+objeto2.vidaRestante);
+
+        //obejto 2 come
+
+        objeto2.Comer(40);
+        System.out.println("Despues de comer 40 points su vida es de \n "+objeto2.vidaRestante);
+
+        System.out.println("Sin embargo, la vida del objeto 1 es la misma, es decir, \n"+objeto1.vidaRestante);
 
 
     }
+
+
 }
